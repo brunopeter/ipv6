e95496e657f214502b5682792512c91a335662f2
Peter Bruno <brunopeter@users.noreply.github.com>
2023-09-20 09:01:36 -0400

    Update nat64toipv4.py
    bug fix and code clean up

--- nat64toipv4.py
+++ nat64toipv4.py
@@ -1,6 +1,6 @@
 '''
 NAT64 to IPv4 Convertor
-2023-04-26 v0.2 Peter Bruno
+2023-09-20 v0.3 Peter Bruno
 
 Example:
   64:ff9b::cc4f:c5db
@@ -10,18 +10,20 @@ Example:
 
 def nat64toipv4(nat64):
   ipv4 = []
-  for nibble in nat64.rsplit(':', 2)[1:3]:
-    # ensure each nible is 4 characters, zero pad if necessary
-    nibble = nibble.zfill(4)
-    # loop through nibbles, convert from hex to bin
-    for x in [0,2]:
-      ipv4.append(str(int(nibble[x:x+2],16)))
+  base16 = 16   # hex (base16) convert to dec
+
+  for nibble in nat64.rsplit(':', 2)[-2:]:
+    nibble = nibble.zfill(4)  # ensure each nible is 4 characters, zero pad if necessary
+    ipv4.append(str(int(nibble[0:2], base16)))
+    ipv4.append(str(int(nibble[2:4], base16)))
 
   return '.'.join(ipv4)
 
 
 if __name__ == '__main__':
-  print('=== NAT64 - IPv4 Convertor ===')
-  addr = input('Enter nat64 address: ')
-
-  print(nat64toipv4(addr))
+  print('=== NAT64 -> IPv4 Convertor - [Q]uit ===')
+  while True:
+    ipv6address = input('Enter nat64 address: ')
+    if ipv6address.upper() == 'Q':
+      break
+    print(nat64toipv4(ipv6address))
